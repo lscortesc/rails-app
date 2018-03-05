1a3b166f756b617f5b669a4e72ef7503810559c2
Luis Cortés <jluis.cortes.cuevas@gmail.com>
2018-03-05 13:04:28 -0600

    Login & Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username].downcase)
+
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        format.html { redirect_to user_path(user), flash: { success: 'You have successfully login.' } }
+        format.json { render :new, status: :created, location: users_path(user) }
+      else
+        format.html { redirect_to login_path, flash: { danger: 'There was something wrong with your login information' } }
+        format.json { render json: user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    respond_to do |format|
+      session[:user_id] = nil
+      format.html { redirect_to root_path, flash: { success: 'You have successfully logout.' } }
+      format.json { render :new, status: :ok, location: root_path }
+    end
   end
 end
