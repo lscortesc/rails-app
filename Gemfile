dcedd82aa1872205c6ad77ef0ba66f2e57518d08
Luis Cortés <jluis.cortes.cuevas@gmail.com>
2018-03-11 12:17:43 -0600

    Create Category with TDD & BDD

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'rails-controller-testing', '~> 1.0', '>= 1.0.2'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,31 @@
-class CategoryController < ApplicationController
+class CategoriesController < ApplicationController
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, flash: { success: "Category '#{@category.name}' created successfully" } }
+      else
+        format.html { render :new, flash: { danger: 'The category was not created' } }
+      end
+    end
   end
 
   def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
